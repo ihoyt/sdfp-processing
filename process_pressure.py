56d6722c9db1ed0a20423d189382a3dfe634f198
Ian Hoyt-McCullough <ianiac@email.unc.edu>
2022-12-07 16:59:13 -0500

    test

--- process_pressure.py
+++ process_pressure.py
@@ -403,13 +403,10 @@ def main():
     print(r.content)
 
     max_date = pd.read_sql_query("SELECT max(date) as date FROM sensor_water_depth", engine)
-    test = max_date.at[0, 'date']
-    test = test + timedelta(days=3)
-    print(test)
-    print(test.strftime("%Y-%m-%d"))
-    return
+    max_date = max_date.at[0, 'date']
+    max_date= test + timedelta(days=3)
     try:
-        new_data = pd.read_sql_query("SELECT * FROM sensor_data WHERE processed = 'FALSE' AND pressure > 800 AND date < '2022-11-12'", engine).sort_values(['place','date']).drop_duplicates()
+        new_data = pd.read_sql_query("SELECT * FROM sensor_data WHERE processed = 'FALSE' AND pressure > 800 AND date < '{max_date}'", engine).sort_values(['place','date']).drop_duplicates()
     except Exception as ex:
         new_data = pd.DataFrame()
         warnings.warn("Connection to database failed to return data")
