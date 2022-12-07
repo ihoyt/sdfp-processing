0a0f5142ab75bb10d6bea81d5cfbf6d5268120aa
Ian Hoyt-McCullough <ianiac@email.unc.edu>
2022-12-07 12:58:52 -0500

    Test

--- process_pressure.py
+++ process_pressure.py
@@ -400,7 +400,7 @@ def main():
     #####################
 
     try:
-        new_data = pd.read_sql_query("SELECT * FROM sensor_data WHERE processed = 'FALSE' AND pressure > 800", engine).sort_values(['place','date']).drop_duplicates()
+        new_data = pd.read_sql_query("SELECT * FROM sensor_data WHERE processed = 'FALSE' AND pressure > 800 AND WHERE date > '2022-12-05", engine).sort_values(['place','date']).drop_duplicates()
     except Exception as ex:
         new_data = pd.DataFrame()
         warnings.warn("Connection to database failed to return data")
